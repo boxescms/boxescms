e420fa68bdf655abca55db6ceb3b5e8770ff64af
JSON object <jsonobject@outlook.com>
2018-06-20 17:50:57 +0800

    Remove imagemin and image compression build

--- helpers/builders/image.js
+++ helpers/builders/image.js
@@ -4,35 +4,48 @@ const {promisify} = require('util')
 const glob = promisify(require('glob'))
 const fs = require('fs')
 const copyFile = promisify(fs.copyFile)
-const writeFile = promisify(fs.writeFile)
+// const writeFile = promisify(fs.writeFile)
 const mkdirp = promisify(require('mkdirp'))
-const imagemin = require('imagemin')
-const imageminJpegtran = require('imagemin-jpegtran')
-const imageminPngquant = require('imagemin-pngquant')
+// const imagemin = require('imagemin')
+// const imageminJpegtran = require('imagemin-jpegtran')
+// const imageminPngquant = require('imagemin-pngquant')
 const chalk = require('chalk')
 
-const buildJpgPng = async () => {
-  const files = await glob(join(base, 'web/**/*.{jpg,png}'))
+// const buildJpgPng = async () => {
+//   const files = await glob(join(base, 'web/**/*.{jpg,png}'))
 
-  return Promise.all(files.map(async file => {
-    const result = await imagemin([file], {
-      plugins: [
-        imageminJpegtran(),
-        imageminPngquant({quality: '65-80'})
-      ]
-    })
+//   return Promise.all(files.map(async file => {
+//     const result = await imagemin([file], {
+//       plugins: [
+//         imageminJpegtran(),
+//         imageminPngquant({quality: '65-80'})
+//       ]
+//     })
 
-    const relativepath = relative(join(base, 'web'), file)
-    const target = join(base, 'public', relativepath)
+//     const relativepath = relative(join(base, 'web'), file)
+//     const target = join(base, 'public', relativepath)
 
-    await mkdirp(dirname(target))
+//     await mkdirp(dirname(target))
 
-    return writeFile(target, result[0].data)
-  }))
-}
+//     return writeFile(target, result[0].data)
+//   }))
+// }
 
-const buildRemainingImages = async () => {
-  const files = await glob(join(base, 'web/**/*.{svg,gif,mp4}'))
+// const buildRemainingImages = async () => {
+//   const files = await glob(join(base, 'web/**/*.{svg,gif,mp4}'))
+
+//   return Promise.all(files.map(async file => {
+//     const relativepath = relative(join(base, 'web'), file)
+//     const target = join(base, 'public', relativepath)
+
+//     await mkdirp(dirname(target))
+
+//     return copyFile(file, target)
+//   }))
+// }
+
+const buildUncompressedImages = async () => {
+  const files = await glob(join(base, 'web/**/*.{jpg,png,svg,gif,mp4}'))
 
   return Promise.all(files.map(async file => {
     const relativepath = relative(join(base, 'web'), file)
@@ -54,7 +67,9 @@ module.exports = async () => {
     return
   }
 
-  await Promise.all([buildJpgPng(), buildRemainingImages()])
+  // await Promise.all([buildJpgPng(), buildRemainingImages()])
+
+  await buildUncompressedImages()
 
   console.log(`Compiled ${chalk.yellow('Images')} ${chalk.blue('[' + (Date.now() - time) + 'ms]')}`)
 }
